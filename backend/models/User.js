c45b2bd8c671cd2bafc82ac6ae0189dac1629926
MenathNDGD <nadungmenath1@gmail.com>
2025-02-12 18:58:36 +0530

    Enhance User model schema by adding required fields and unique constraint for email

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   role: { type: String, enum: ["admin", "employee"], default: "employee" },
 });
 
